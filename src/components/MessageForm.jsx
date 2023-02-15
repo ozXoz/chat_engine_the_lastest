00827dab7ccbbbae9c5d11c16909d463e4035c71
ozXoz <honourkorkmaz@gmail.com>
2023-02-14 21:19:11 -0500

    fix the bug in message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,5 @@
 import {useState} from 'react';
 import { sendMessage,isTyping } from 'react-chat-engine';
-import {SendOutlined,PictureOutlined} from '@ant-design/icons'
 const MessageForm = (props)=>{
     const[value,setValue]=useState('');
     const {chatId,creds}=props;
